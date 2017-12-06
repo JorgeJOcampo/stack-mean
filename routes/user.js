cc7d340d7f81455d349e693949073cc09c1b74a6
JorgeJOcampo <caramelos1>
2017-12-05 23:48:13 -0300

    controller de album

--- routes/user.js
+++ routes/user.js
@@ -9,8 +9,8 @@ var md_auth = require('../middlewares/authenticated');
 var multipart = require('connect-multiparty');
 var md_upload = multipart({ uploadDir: './uploads/users'});
 
-api.get('/prueba', md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
+api.get('/prueba', md_auth.ensureAuth, UserController.pruebas);
 api.post('/login', UserController.loginUser);
 api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
 api.post('/upload-image-user/:id', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
